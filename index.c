7f55b96faa113c1e5dfc291709e6eb52d1a58c07
MarceloPrgJr <josemarceloisidorio@gmail.com>
2020-06-20 22:14:43 -0300

    Descrição

---
+++ index.c
@@ -0,0 +1,16 @@
+/*
+*   O programa tem a finalidade de fazer os cálculos de bhaskara para o usuário
+*  a partir dos valores recebidos, apresentando os possíveis resultados ao decorrer do código.
+*
+*   @autor: Niiick013
+*   @GitHub: https://github.com/Niiick013
+*
+*/
+#include <stdio.h>
+
+int main() {
+
+
+    return 0;
+
+}
